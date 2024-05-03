594cc706fe6e4ff813d10a537d90d6005ebb0aa0
Y0rha9s <fra.castroa@gmail.com>
2024-05-03 16:09:40 -0400

    comente cosas dela rchivo url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
-from rest_framework import routers
+#from rest_framework import routers
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'agendamientos', views.AgendamientoViewsets)
+#router = routers.DefaultRouter()
+#router.register(r'agendamientos', views.AgendamientoViewsets)
 
-urlpatterns=[
-    path('', include(router.urls))
-]
+#urlpatterns=[
+#    path('', include(router.urls))
+#]
